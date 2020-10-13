ef3a5b69a015af6ed729fc8bfc46994aafeca8cc
Talha Imran <talhaimran284@gmail.com>
2020-10-13 14:32:31 +0500

    Implemented drop down functionality for the contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react'
 
 export class Contact extends Component {
+
+    state = {
+        showContact: false
+    }
+
+    showContactHandler = () => {
+        this.setState({
+            showContact: !this.state.showContact
+        });
+    }
     render() {
         return (
 
             <div className="card card-body my-3">
                 <h3>{this.props.name} 
-                    <i className="fas fa-chevron-circle-down ml-3 text-success" style = {{cursor: "pointer"}}></i>
-
                     <i className="fas fa-backspace text-danger mr-5" style = {{cursor: "pointer", float: "right"}}></i> 
+                
+                    <i className="fas fa-chevron-circle-down mr-5 text-success" onClick = {this.showContactHandler} style = {{cursor: "pointer", float: "right"}}></i>
                 </h3>
 
+                {/* Conditionaly reders this contact information */}
 
-                 <ul className="list-group">
-                    <li className="list-group-item">
-                        {this.props.email}
-                    </li>
 
-                    <li className="list-group-item">
-                        {this.props.phone}
-                    </li>
-                </ul>
+                {this.state.showContact ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            {this.props.email}
+                        </li>
+
+                        <li className="list-group-item">
+                            {this.props.phone}
+                        </li>
+                    </ul>
+                ) : null }
             </div>
         )
     }
