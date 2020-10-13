3ca14ffec01c54f2dcff2e85a3008446fd7d4942
Talha Imran <talhaimran284@gmail.com>
2020-10-13 14:22:34 +0500

    Added dropdown and delete button icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,13 @@ export class Contact extends Component {
         return (
 
             <div className="card card-body my-3">
-                <h3>{this.props.name}</h3>
+                <h3>{this.props.name} 
+                    <i className="fas fa-chevron-circle-down ml-3 text-success" style = {{cursor: "pointer"}}></i>
+
+                    <i className="fas fa-backspace text-danger mr-5" style = {{cursor: "pointer", float: "right"}}></i> 
+                </h3>
+
+
                  <ul className="list-group">
                     <li className="list-group-item">
                         {this.props.email}
